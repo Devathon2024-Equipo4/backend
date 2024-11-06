cc5e4539e24095e464db94d67199da17f2aaebf1
joams mogro gomez <joamsmg@gmail.com>
2024-11-06 12:52:00 -0400

    feat: merge with develop

--- routes/index.ts
+++ routes/index.ts
@@ -6,11 +6,14 @@ import CaloriesModel from "../models/calories";
 import { elfRoutes } from "./elf";
 import ElfModel from "../models/elf";
 import { weatherRoutes } from "./weather";
+import { letterRoutes } from "./letter";
+import LetterModel from "../models/letter";
+
 export const registerRoutes = (app: Express, apiVersion: string) => {
     
     app.use(`${apiVersion}/reindeers`, reindeerRoutes(ReindeerModel));
     app.use(`${apiVersion}/calories`, caloriesRoutes(CaloriesModel));
     app.use(`${apiVersion}/elves`, elfRoutes(ElfModel));
     app.use(`${apiVersion}/weather`, weatherRoutes(ReindeerModel));
-
+    app.use(`${apiVersion}/letter`, letterRoutes(LetterModel));
 }
