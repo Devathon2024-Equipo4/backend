f64fa49fd20a12a04f0a5d72cefd56dd426ac855
joams mogro gomez <joamsmg@gmail.com>
2024-11-06 12:52:00 -0400

    feat: letter routes added into main router

--- routes/index.ts
+++ routes/index.ts
@@ -4,10 +4,13 @@ import ReindeerModel from "../models/reindeer";
 import { caloriesRoutes } from "./calories";
 import CaloriesModel from "../models/calories";
 import { weatherRoutes } from "./weather";
+import { letterRoutes } from "./letter";
+import LetterModel from "../models/letter";
 
 export const registerRoutes = (app: Express, apiVersion: string) => {
     
     app.use(`${apiVersion}/reindeers`, reindeerRoutes(ReindeerModel));
     app.use(`${apiVersion}/calories`, caloriesRoutes(CaloriesModel));
     app.use(`${apiVersion}/weather`, weatherRoutes(ReindeerModel));
+    app.use(`${apiVersion}/letter`, letterRoutes(LetterModel));
 }
