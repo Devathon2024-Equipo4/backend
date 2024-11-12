8e8718b76665ab00bbef0b4a6e7b4f878cdab280
joams mogro gomez <joamsmg@gmail.com>
2024-11-11 21:49:12 -0400

    fix: some migrations were removed (these cause a minor bug)

--- prisma/migrations/20241104200604_reindeer/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Reindeer" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "alignment" INTEGER NOT NULL,
-
-    CONSTRAINT "Reindeer_pkey" PRIMARY KEY ("id")
-);
